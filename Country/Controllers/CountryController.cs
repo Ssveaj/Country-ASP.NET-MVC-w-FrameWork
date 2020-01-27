04090b9db52e4d2610fb374a4154639af9db9d10
Joel Ferm <joel.ferm@learnet.se>
2020-01-27 10:29:31 +0100

    Add project files.

---
+++ Country/Controllers/CountryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Country.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET: Country
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "India",
+                "Sweden",
+                "China",
+                "USA"
+            };
+            return View();
+        }
+    }
+}
---
+++ Country/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Country.MvcApplication" Language="C#" %>
